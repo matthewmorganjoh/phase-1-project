02104e0c9b3829a8cdd9f0dd5a7acd0f2c70d669
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-05-11 11:11:08 -0700

    create event listener and API access

--- index.js
+++ index.js
@@ -9,33 +9,38 @@ searchForm.addEventListener('submit', function(event) {
   const formattedName = name.replace(/ /g, "_");
 
   fetch(`https://botw-compendium.herokuapp.com/api/v2/entry/${formattedName}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Error occurred.');
+      }
+      return response.json();
+    })
     .then(data => {
-      // Process the API response data
+      if (data.error) {
+        throw new Error(data.error);
+      }
       const product = data.data;
 
-      // Update the UI with the product information
       resultsContainer.innerHTML = '';
-      errorMessage.textContent = ''; // Clear any previous error messages
+      errorMessage.textContent = '';
 
-      // Create elements to display the product information
       const productName = document.createElement('h2');
       productName.textContent = product.name;
 
       const productImage = document.createElement('img');
       productImage.src = product.image;
 
-      // Append the elements to the results container
+
+      const productDescription = document.createElement('h3');
+      productDescription.textContent = product.description;
+
       resultsContainer.appendChild(productName);
       resultsContainer.appendChild(productImage);
+      resultsContainer.appendChild(productDescription);
     })
     .catch(error => {
       console.error('Error:', error);
-      resultsContainer.innerHTML = ''; // Clear any existing results
-      errorMessage.textContent = 'Error occurred. Please try again later.'; // Display error message
+      resultsContainer.innerHTML = '';
+      errorMessage.textContent = error.message;
     });
 });
-
-
-
-
