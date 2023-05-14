30409fcb4032c6d4d76df8d766bdb7c19869c49f
Matthew Morgan <matthewmorganjoh@gmail.com>
2023-05-14 14:08:52 -0700

    Remove whitespace, comment the most important blocks

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const loadingState = document.getElementById('loading-state');
 const clearButton = document.getElementById('clear-button');
 const previouslyViewedContainer = document.getElementById('previously-viewed-container'); 
 
-const MAX_PREVIOUSLY_VIEWED = 4;
-let previouslyViewedItems = []; // Store previously viewed items
+const MAX_PREVIOUSLY_VIEWED = 5;
+let previouslyViewedItems = [];
 
+// Event listener 1 for the "submit" button, main container for the code
 searchForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
   const name = event.target.name.value;
-  const formattedName = name.replace(/ /g, "_");
+  const formattedName = name.replace(/ /g, "_").toLowerCase(); // API has underscores for all values with a space. Adding in an underscore so the user will not have to type this in
 
   loadingState.style.display = 'block';
   resultsContainer.innerHTML = '';
@@ -21,7 +22,7 @@ searchForm.addEventListener('submit', function(event) {
   fetch(`https://botw-compendium.herokuapp.com/api/v2/entry/${formattedName}`)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Error occurred. Search was not found at https://botw-compendium.herokuapp.com/api/v2');
+        throw new Error('Error occurred. Search was not found at https://botw-compendium.herokuapp.com/api/v2'); // throw error immediately if the data is not in the API
       }
       return response.json();
     })
@@ -29,36 +30,30 @@ searchForm.addEventListener('submit', function(event) {
       if (data.error) {
         throw new Error(data.error);
       }
+
+      // display elements from HTML file
       const product = data.data;
-
-      loadingState.style.display = 'none'; // Hide loading 
-
+      loadingState.style.display = 'none';
       const productName = document.createElement('h2');
       productName.textContent = product.name;
-
       const productImage = document.createElement('img');
       productImage.src = product.image;
-
       const productDescription = document.createElement('h3');
       productDescription.textContent = product.description;
-
       resultsContainer.appendChild(productName);
       resultsContainer.appendChild(productImage);
       resultsContainer.appendChild(productDescription);
-
       addToPreviouslyViewed(formattedName);
-
       updatePreviouslyViewed();
     })
     .catch(error => {
       console.error('Error:', error);
       resultsContainer.innerHTML = '';
       errorMessage.textContent = error.message;
-
       loadingState.style.display = 'none';
     });
 });
-
+// Event listener 2 for the "clear" button
 clearButton.addEventListener('click', function() {
   searchForm.reset();
   resultsContainer.innerHTML = '';
@@ -66,7 +61,7 @@ clearButton.addEventListener('click', function() {
   previouslyViewedContainer.innerHTML = '';
   previouslyViewedItems = [];
 });
-
+// Event listener 3 for the previously viewed section
 previouslyViewedContainer.addEventListener('click', function(event) {
   if (event.target.classList.contains('previously-viewed-item')) {
     const clickedItem = event.target.dataset.item;
@@ -76,36 +71,29 @@ previouslyViewedContainer.addEventListener('click', function(event) {
     }
   }
 });
-
+// Add image to previously viewed section
 function addToPreviouslyViewed(item) {
   previouslyViewedItems = previouslyViewedItems.filter(prevItem => prevItem !== item);
-
   previouslyViewedItems.unshift(item);
-
   if (previouslyViewedItems.length > MAX_PREVIOUSLY_VIEWED) {
     previouslyViewedItems.pop();
   }
 };
-
+//Update previously viewed section with proper item name and image
 function updatePreviouslyViewed() {
   previouslyViewedContainer.innerHTML = '';
-
   previouslyViewedItems.forEach(item => {
     const img = document.createElement('img');
     img.src = `https://botw-compendium.herokuapp.com/api/v2/entry/${item}/image`;
     img.alt = 'Previously Viewed';
     const itemName = item.replace(/_/g, ' ');
-
     const name = document.createElement('p');
     name.textContent = itemName;
-
-
     const itemContainer = document.createElement('div');
     itemContainer.classList.add('previously-viewed-item');
     itemContainer.dataset.item = item;
     itemContainer.appendChild(name);
     itemContainer.appendChild(img);
-
     previouslyViewedContainer.appendChild(itemContainer);
   });
 };
